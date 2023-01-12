964b6a7f9f492520258cf604fac385facd967e44
RepoKishor <imkishorcs@gmail.com>
2023-01-12 19:11:05 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import pymongo
+from thyroid.utils import get_collection_as_dataframe
+from thyroid.logger import logging
+from thyroid.exception import ThyroidException
+from thyroid.entity import config_entity
+from thyroid.entity.config_entity import DataIngestionConfig
+from thyroid.components.data_ingestion import DataIngestion
+import sys,os
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+if __name__ =="__main__":
+     try:
+          #get_collection_as_dataframe(database_name="thyroid",collection_name="detectthyroid")
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion= DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
+     except Exception  as e:
+          print(e)
+     
 
-# Database Name
-dataBase = client["neurolabDB"]
-
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
