3750755405ecf0a4b5ffdf15a546b6c2d0440637
RepoKishor <imkishorcs@gmail.com>
2023-01-18 12:08:07 +0530

    batch prediction

--- airflow/dags/batch_prediction.py
+++ airflow/dags/batch_prediction.py
@@ -8,7 +8,7 @@ from airflow.operators.python import PythonOperator
 
 
 with DAG(
-    'thyroid_training',
+    'batch_prediction',
     default_args={'retries': 2},
     # [END default_args]
     description='Thyroid Disease Detection',
