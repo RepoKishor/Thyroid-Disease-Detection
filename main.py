502394bbe375e2b0890c3bb00fbab82a872e23c9
RepoKishor <imkishorcs@gmail.com>
2023-01-18 12:21:31 +0530

    Updated main method and the train.py

--- main.py
+++ main.py
@@ -1,55 +1,73 @@
-import pymongo
-from thyroid.utils import get_collection_as_dataframe
-from thyroid.logger import logging
-from thyroid.exception import ThyroidException
-from thyroid.entity import config_entity
-from thyroid.entity.config_entity import DataIngestionConfig
-from thyroid.components.data_ingestion import DataIngestion
-from thyroid.components.data_validation import DataValidation
-from thyroid.components.data_transformation import DataTransformation
-from thyroid.components.model_trainer import ModelTrainer
-from thyroid.components.model_evaluation import ModelEvaluation
-from thyroid.components.model_pusher import ModelPusher
-import sys,os
+from thyroid.pipeline.training_pipeline import start_training_pipeline
+from thyroid.pipeline.batch_prediction import start_batch_prediction
 
-if __name__ =="__main__":
+file_path="/config/workspace/hypothyroid.csv"
+print(__name__)
+if __name__=="__main__":
      try:
-          #get_collection_as_dataframe(database_name="thyroid",collection_name="detectthyroid")
-          training_pipeline_config = config_entity.TrainingPipelineConfig()
-          data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-          print(data_ingestion_config.to_dict())
-          data_ingestion= DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
-          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
-          data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
-
-          data_validation_artifact = data_validation.initiate_data_validation()
-
-          data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
-          data_transformation = DataTransformation(data_transformation_config=data_transformation_config,data_ingestion_artifact=data_ingestion_artifact)
-          data_transformation_artifact = data_transformation.initiate_data_transformation()
-
-          #model trainer
-          model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
-          model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
-          model_trainer_artifact = model_trainer.initiate_model_trainer()
-
-          # model evaluation
-          model_eval_config= config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
-          model_eval = ModelEvaluation(model_eval_config=model_eval_config,
-          data_ingestion_artifact = data_ingestion_artifact,
-          data_transformation_artifact= data_transformation_artifact,
-          model_trainer_artifact = model_trainer_artifact)
-
-          model_eval.initiate_model_evaluation()          
-
-          model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
-          model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
-                              data_transformation_artifact = data_transformation_artifact,
-                              model_trainer_artifact = model_trainer_artifact)
-          model_pusher_artifact = model_pusher.initiate_model_pusher()
-
-
-     except Exception  as e:
+          #start_training_pipeline()
+          output_file = start_batch_prediction(input_file_path=file_path)
+          print(output_file)
+     except Exception as e:
           print(e)
+
+
+
+
+
+
+# import pymongo7
+# from thyroid.utils import get_collection_as_dataframe
+# from thyroid.logger import logging
+# from thyroid.exception import ThyroidException
+# from thyroid.entity import config_entity
+# from thyroid.entity.config_entity import DataIngestionConfig
+# from thyroid.components.data_ingestion import DataIngestion
+# from thyroid.components.data_validation import DataValidation
+# from thyroid.components.data_transformation import DataTransformation
+# from thyroid.components.model_trainer import ModelTrainer
+# from thyroid.components.model_evaluation import ModelEvaluation
+# from thyroid.components.model_pusher import ModelPusher
+# import sys,os
+
+# if __name__ =="__main__":
+#      try:
+#           #get_collection_as_dataframe(database_name="thyroid",collection_name="detectthyroid")
+#           training_pipeline_config = config_entity.TrainingPipelineConfig()
+#           data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+#           print(data_ingestion_config.to_dict())
+#           data_ingestion= DataIngestion(data_ingestion_config=data_ingestion_config)
+#           print(data_ingestion.initiate_data_ingestion())
+#           data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+#           data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+#           data_validation = DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact=data_ingestion_artifact)
+
+#           data_validation_artifact = data_validation.initiate_data_validation()
+
+#           data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+#           data_transformation = DataTransformation(data_transformation_config=data_transformation_config,data_ingestion_artifact=data_ingestion_artifact)
+#           data_transformation_artifact = data_transformation.initiate_data_transformation()
+
+#           #model trainer
+#           model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+#           model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+#           model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+#           # model evaluation
+#           model_eval_config= config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+#           model_eval = ModelEvaluation(model_eval_config=model_eval_config,
+#           data_ingestion_artifact = data_ingestion_artifact,
+#           data_transformation_artifact= data_transformation_artifact,
+#           model_trainer_artifact = model_trainer_artifact)
+
+#           model_eval.initiate_model_evaluation()          
+
+#           model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
+#           model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
+#                               data_transformation_artifact = data_transformation_artifact,
+#                               model_trainer_artifact = model_trainer_artifact)
+#           model_pusher_artifact = model_pusher.initiate_model_pusher()
+
+
+#      except Exception  as e:
+#           print(e)
